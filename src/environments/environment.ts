f7236dd09d0c6df75ce0bfed44aed12e1f3c41c2
Quang Huy Nguyen <quanghuy_nguyen@datahouse.com>
2023-02-14 22:59:33 +0700

    setup firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyD4kpQmrTw1-9lo9hKtIJq0UU3-iHsN3fo',
+    authDomain: 'rmd-project-51269.firebaseapp.com',
+    databaseURL: '<your-database-URL>',
+    projectId: 'rmd-project-51269',
+    storageBucket: 'rmd-project-51269.appspot.com',
+    messagingSenderId: '919341790965',
+    appId: '1:919341790965:web:40f961b4a51b9e5363b7bc',
+    measurementId: '<your-measurement-id>',
+  },
+};
